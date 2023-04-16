6d04f0d1f77928a51a0160af589d3967c461dbf5
hangchen <hangchen.xie@uzh.ch>
2023-04-16 19:15:12 +0200

    add new api authorization

---
+++ src/helpers/auth.js
@@ -0,0 +1,23 @@
+import {useEffect, useState} from "react";
+import {useRouter} from "next/router";
+import {message} from "antd";
+import Cookies from 'js-cookie'
+
+export default function useAuth() {
+  const [authenticated, setAuthenticated] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    // Check if token is stored in cookie or local storage
+    const token = Cookies.get('token');
+
+    if (!token) {
+      // User is not authenticated, redirect to login page
+      router.push('/login');
+    } else {
+      setAuthenticated(true)
+    }
+  }, []);
+
+  return authenticated;
+}
