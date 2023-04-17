4f5c69b1a25958bfae73304366de8666027d85f3
hangchen <hangchen.xie@uzh.ch>
2023-04-17 18:54:54 +0200

    add layout

---
+++ src/components/ui/layout.js
@@ -0,0 +1,21 @@
+import Header from "@/components/ui/Header";
+import Footer from "@/components/ui/Footer";
+import {Layout} from "antd";
+
+const ILayout = ({children}) => {
+    return (
+        <Layout>
+            <Layout.Header style={{ padding: 0}}>
+                <Header/>
+            </Layout.Header>
+            <Layout.Content>
+                <main> {children} </main>
+            </Layout.Content>
+            <Layout.Footer style={{ padding: 0 }}>
+                <Footer/>
+            </Layout.Footer>
+        </Layout>
+    );
+};
+
+export default ILayout;
