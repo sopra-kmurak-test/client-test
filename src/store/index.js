5841493c4a8070145806164dd4da87c7f7b66566
hangchen <hangchen.xie@uzh.ch>
2023-04-25 12:33:19 +0200

    change file location

--- src/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "@/store/modules/user";
-
-export default configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
