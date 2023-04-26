f89a886aee0ee7b0f15dbd305e9b2f3ff4e9df9d
hangchen <hangchen.xie@uzh.ch>
2023-04-26 23:53:25 +0200

    change create

---
+++ src/components/views/Question/Create.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Button, Card, Form, Input } from "antd";
+import styles from "styles/views/question.create.module.scss";
+import { newQuestion } from "helpers/api/question";
+import useAuth from "helpers/api/auth";
+
+const Create = () => {
+  useAuth();
+
+  const handleCreate = (values) => {
+    if (!values.detail) {
+      values["detail"] = "";
+    }
+
+    newQuestion(values).then((response) => {
+      if (response.success === true) {
+        console.log("Create question successfully.");
+      }
+    });
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <p className={styles.title}>Create Your Own Question</p>
+        <Card bordered style={{ width: "560px" }}>
+          <Form onFinish={handleCreate}>
+            <Form.Item
+              name={"title"}
+              rules={[
+                { required: true, message: "please input question title." },
+              ]}
+            >
+              <Input size={"large"} placeholder={"Your Question Title"} />
+            </Form.Item>
+
+            <Form.Item name={"detail"}>
+              <Input.TextArea
+                size={"large"}
+                placeholder={"Description (optional)"}
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                style={{ backgroundColor: "#6F3BF5" }}
+                size={"large"}
+                type={"primary"}
+                htmlType={"submit"}
+              >
+                Create
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default Create;
