e010b6567da354a14fd7a16d4661bffdcf3cc549
1316827294 <43595374+1316827294@users.noreply.github.com>
2023-04-26 22:46:44 +0200

    change routers

--- src/components/routing/routeProtectors/QuestionGuard.js
+++ src/components/routing/routeProtectors/QuestionGuard.js
@@ -12,7 +12,7 @@ import PropTypes from "prop-types";
  */
 export const QuestionGuard = props => {
     if (!localStorage.getItem("token")) {
-        console.log(localStorage.getItem("token"));
+        console.log(document.cookie+"---------");
         return props.children;//
     }
     // if user is already logged in, redirects to the main /app
