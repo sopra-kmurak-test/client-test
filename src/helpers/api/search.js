fdbae6f996b2717e774579931d3696008719b8a3
hangchen <hangchen.xie@uzh.ch>
2023-04-27 19:56:59 +0200

    change api configuration

--- src/helpers/api/search.js
+++ src/helpers/api/search.js
@@ -1,7 +1,7 @@
 import requests from "./requests_utils";
 
 export function search(params) {
-    return requests.get(`/search/preSearch`, {//?q=${params.q}
+    return requests.get(`/search/`, {//?q=${params.q}
         params: params
     });
 }
