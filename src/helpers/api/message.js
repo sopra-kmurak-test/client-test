08b3425f45d020eabc522f1178d9c5fd327611fd
hangchen <hangchen.xie@uzh.ch>
2023-04-27 19:55:47 +0200

    change api configuration

--- src/helpers/api/message.js
+++ src/helpers/api/message.js
@@ -1,9 +1,9 @@
 import requests from "./requests_utils";
-
+//
 export function insertMessage(data) {
-    return requests.post(`/message/insert?content=${data.content}&fromUserId=${data.fromUserId}&toUserId=${data.toUserId}`, data);
+    return requests.post(`/message/insert?content=${data.content}&fromUserId=${data.fromUserId}&toUserId=${data.toUserId}`);
 }
-
+//
 export function listMessage(data) {
-    return requests.post(`/message/list?fromUserId=${data.fromUserId}&toUserId=${data.toUserId}`, data);
+    return requests.post(`/message/list?fromUserId=${data.fromUserId}&toUserId=${data.toUserId}`);
 }
