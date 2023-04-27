148fac8c4f5b688a2279cfafce337f94264d81c4
hangchen <hangchen.xie@uzh.ch>
2023-04-27 20:11:45 +0200

    change create

--- src/components/views/Question/Create.js
+++ src/components/views/Question/Create.js
@@ -5,57 +5,57 @@ import { newQuestion } from "helpers/api/question";
 import useAuth from "helpers/api/auth";
 
 const Create = () => {
-  useAuth();
+    useAuth();
 
-  const handleCreate = (values) => {
-    if (!values.detail) {
-      values["detail"] = "";
-    }
+    const handleCreate = (values) => {
+        if (!values.detail) {
+            values["detail"] = "";
+        }
 
-    newQuestion(values).then((response) => {
-      if (response.success === true) {
-        console.log("Create question successfully.");
-      }
-    });
-  };
+        newQuestion(values).then((response) => {
+            // console.log(response)
+            if (response.success === 'true') {
+                // console.log("创建成功");
+                window.location.replace(response.userID);
+            }
+        });
+    };
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.main}>
-        <p className={styles.title}>Create Your Own Question</p>
-        <Card bordered style={{ width: "560px" }}>
-          <Form onFinish={handleCreate}>
-            <Form.Item
-              name={"title"}
-              rules={[
-                { required: true, message: "please input question title." },
-              ]}
-            >
-              <Input size={"large"} placeholder={"Your Question Title"} />
-            </Form.Item>
+    return (
+        <div className={styles.container}>
+            <div className={styles.main}>
+                <p className={styles.title}>Create Your Own Question</p>
+                <Card bordered style={{ width: "560px" }}>
+                    <Form onFinish={handleCreate}>
+                        <Form.Item
+                            name={"title"}
+                            rules={[{ required: true, message: "please input question title." }]}
+                        >
+                            <Input size={"large"} placeholder={"Your Question Title"} />
+                        </Form.Item>
 
-            <Form.Item name={"detail"}>
-              <Input.TextArea
-                size={"large"}
-                placeholder={"Description (optional)"}
-              />
-            </Form.Item>
+                        <Form.Item name={"detail"}>
+                            <Input.TextArea
+                                size={"large"}
+                                placeholder={"Description (optional)"}
+                            />
+                        </Form.Item>
 
-            <Form.Item>
-              <Button
-                style={{ backgroundColor: "#6F3BF5" }}
-                size={"large"}
-                type={"primary"}
-                htmlType={"submit"}
-              >
-                Create
-              </Button>
-            </Form.Item>
-          </Form>
-        </Card>
-      </div>
-    </div>
-  );
+                        <Form.Item>
+                            <Button
+                                style={{ backgroundColor: "#6F3BF5" }}
+                                size={"large"}
+                                type={"primary"}
+                                htmlType={"submit"}
+                            >
+                                Create
+                            </Button>
+                        </Form.Item>
+                    </Form>
+                </Card>
+            </div>
+        </div>
+    );
 };
 
 export default Create;
