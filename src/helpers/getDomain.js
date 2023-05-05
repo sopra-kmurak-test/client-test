18f12e7c5c882ad6f346b55d951e9bd8de44ec9a
kmkm0113 <k.murakami0113@gmail.com>
2023-05-05 14:30:03 +0200

    first commit

--- src/helpers/getDomain.js
+++ src/helpers/getDomain.js
@@ -7,8 +7,8 @@ import { isProduction } from 'helpers/isProduction'
  * @returns {string}
  */
 export const getDomain = () => {
-  const prodUrl = 'https://sopra-fs23-group-38-server.oa.r.appspot.com/' // TODO: insert your prod url for server (once deployed)
-  const devUrl = 'https://sopra-fs23-group-38-server.oa.r.appspot.com/'
+  const prodUrl = 'https://sopra-kmurak-server-test.oa.r.appspot.com/' // TODO: insert your prod url for server (once deployed)
+  const devUrl = 'http://localhost:8080'
 
   return isProduction() ? prodUrl : devUrl
 }
